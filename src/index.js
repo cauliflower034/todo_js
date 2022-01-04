63ddad006363c554170070ec3b2b7eb571349ef9
Shingo <93265847+cauliflower034@users.noreply.github.com>
2022-01-04 12:12:30 +0900

    listへの追加

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import "./styles.css";
+
+const addtxt = document.getElementById("add-text");
+const addbtn = document.getElementById("add-button");
+const incmp = document.getElementById("incomp-list");
+
+const onClickAdd = () => {
+  const inputText = addtxt.value;
+  addtxt.value = "";
+
+  //div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  //li生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  //divの子要素設定
+  div.appendChild(li);
+
+  //ul下にdiv生成
+  incmp.appendChild(div);
+};
+
+addbtn.addEventListener("click", () => onClickAdd());
